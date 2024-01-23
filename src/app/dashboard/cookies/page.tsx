8fe89d7039669f55ab87e7f64a87e4666d2e454b
wcarbajal <wcarbajal@outlook.com>
2024-01-23 14:22:30 -0500

    tarde del 23.01.2024

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,4 +1,8 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import { TabBar } from "@/components";
+import { cookies } from 'next/headers'
 
 export const metadata = {
   title: 'Cookies page',
@@ -6,12 +10,16 @@ export const metadata = {
 }
 
 export default function CookiesPage() {
+
+    const cookieStore = cookies();
+    const cookieTab = cookieStore.get('selectedTab')?.value ?? '1'; 
+    
   return (
     <div className="grid grid-cols-1 sm:grid-col-2 gap-3">
 
       <div className="flex flex-col">
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar currentTab={ +cookieTab } />
       </div>
 
 
--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { setCookie } from "cookies-next";
 import { useState } from "react";
 
 // https://tailwindcomponents.com/component/radio-buttons-1
@@ -10,20 +11,35 @@ interface Props {
 
 }
 
-export const TabBar = ({tabOptions = [1, 2, 3, 4, 5, 6], currentTab: currentTab = 1}: Props) => {
+export const TabBar = ({tabOptions = [1, 2, 3, 4], currentTab: currentTab = 1}: Props) => {
+
+    let numeroCol = tabOptions.length;
+    
 
   const [selected, setSelected] = useState(currentTab)
+
+  const onTabSelected = ( tab: number) => {
+    setSelected( tab );
+    setCookie('selectedTab', tab.toString());
+
+  }
+
   return (
     <div className= {`
         grid w-full space-x-2 rounded-xl bg-gray-200 p-2
-        ${ 'grid-cols-' + tabOptions.length }
+        grid-cols-4
         `}>
 
       {
         tabOptions.map( tab => (
           <div key={tab}>
-            <input type="radio" id={ tab.toString() } className="peer hidden" />
-            <label className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">
+            <input
+                checked = { selected === tab}
+                onChange={ () => {}}
+                type="radio"
+                id={ tab.toString() }
+                className="peer hidden" />
+            <label onClick={ () => onTabSelected(tab) } className=" transition-all block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">
               {tab}
             </label>
           </div>
