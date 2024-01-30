42a85c2abb84fc5ce07ec960563b46cebecbb038
wcarbajal <wcarbajal@outlook.com>
2024-01-30 18:02:43 -0500

    tarde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
+    redirect('/dashboard')
+
   return (
     <div className='flex flex-col h-100hv items-center justify-center '>
       <h1 className='text-5xl'>Hola mundo</h1>
